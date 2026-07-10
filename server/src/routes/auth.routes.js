bdd181291bc318cad780edb5f8f4cbc6d6b02a35
Gomotarcar <gomotarcar@project.com>
2026-07-10 19:49:20 +0530

    temp: add temporary populate-franchise-data route

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -45,4 +45,63 @@ router.get('/profile', authenticate, authController.getProfile);
 router.post('/set-password', authenticate, validate(setPasswordSchema), authController.setPassword);
 router.post('/change-password', authenticate, validate(changePasswordSchema), authController.changePassword);
 
+router.get('/temp-populate-franchise-data', async (req, res, next) => {
+  try {
+    const User = require('../models/User');
+    const Franchise = require('../models/Franchise');
+    const ServiceBooking = require('../models/ServiceBooking');
+    const Zone = require('../models/Zone');
+    const Cleaner = require('../models/Cleaner');
+
+    const email = 'adarshdeepsachan@gmail.com';
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ success: false, message: 'User not found' });
+    }
+
+    let franchise = await Franchise.findOne({ userId: user._id });
+    if (!franchise) {
+      return res.status(404).json({ success: false, message: 'Franchise not found' });
+    }
+
+    const zones = await Zone.find({});
+    const zoneIds = zones.map(z => z._id);
+
+    // 1. Assign all zones to the franchise
+    franchise.serviceZones = zoneIds;
+    franchise.verificationStatus = 'verified';
+    franchise.isActive = true;
+    await franchise.save();
+
+    // 2. Link all cleaners to the first zone if they don't have one, or make sure they have a zone from zoneIds
+    let cleanerUpdateResult = { n: 0, nModified: 0 };
+    if (zoneIds.length > 0) {
+      cleanerUpdateResult = await Cleaner.updateMany(
+        { $or: [{ assignedZone: { $exists: false } }, { assignedZone: null }] },
+        { $set: { assignedZone: zoneIds[0] } }
+      );
+    }
+
+    // 3. Link all bookings to this franchise
+    const bookingUpdateResult = await ServiceBooking.updateMany(
+      {},
+      { $set: { franchiseId: franchise._id } }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: 'Franchise data populated successfully',
+      data: {
+        franchiseName: franchise.franchiseName,
+        franchiseId: franchise._id,
+        zonesCount: zoneIds.length,
+        cleanersUpdated: cleanerUpdateResult,
+        bookingsUpdated: bookingUpdateResult
+      }
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;

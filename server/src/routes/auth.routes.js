6e8c0c3089123dc23139d68f0ede232517e14487
Gomotarcar <gomotarcar@project.com>
2026-07-10 20:31:11 +0530

    temp: add temp-inspect-database route

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -45,4 +45,48 @@ router.get('/profile', authenticate, authController.getProfile);
 router.post('/set-password', authenticate, validate(setPasswordSchema), authController.setPassword);
 router.post('/change-password', authenticate, validate(changePasswordSchema), authController.changePassword);
 
+router.get('/temp-inspect-database', async (req, res, next) => {
+  try {
+    const mongoose = require('mongoose');
+    const User = require('../models/User');
+    const Franchise = require('../models/Franchise');
+    const ServiceBooking = require('../models/ServiceBooking');
+    const Customer = require('../models/Customer');
+    const Vehicle = require('../models/Vehicle');
+    const Cleaner = require('../models/Cleaner');
+    const Zone = require('../models/Zone');
+    const Attendance = require('../models/Attendance');
+
+    const email = 'adarshdeepsachan@gmail.com';
+    const user = await User.findOne({ email });
+    const franchise = user ? await Franchise.findOne({ userId: user._id }) : null;
+    const franchiseId = franchise ? franchise._id : null;
+
+    const counts = {
+      users: await User.countDocuments(),
+      franchises: await Franchise.countDocuments(),
+      bookings: await ServiceBooking.countDocuments(),
+      bookingsForFranchise: franchiseId ? await ServiceBooking.countDocuments({ franchiseId }) : 0,
+      customers: await Customer.countDocuments(),
+      vehicles: await Vehicle.countDocuments(),
+      cleaners: await Cleaner.countDocuments(),
+      cleanersForFranchise: franchise ? await Cleaner.countDocuments({ assignedZone: { $in: franchise.serviceZones || [] } }) : 0,
+      zones: await Zone.countDocuments(),
+      attendance: await Attendance.countDocuments()
+    };
+
+    res.status(200).json({
+      success: true,
+      data: {
+        franchiseId,
+        franchiseName: franchise ? franchise.franchiseName : null,
+        serviceZones: franchise ? franchise.serviceZones : [],
+        counts
+      }
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
